2147f7434c741680a28d4c7a8e391f0b341286b3
Shreya Puthi <shreya.x.puthi.-nd@disney.com>
2025-08-20 13:09:26 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: 1
+          {/* Application version: 1 */}
+          Application version: {process.env.REACT_APP_VERSION}
       </p>
     </div>
   );
